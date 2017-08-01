5b6ea44eea47ede51e65edb27b4a2b6333e2e621
Alessandro Molari <molari.alessandro@gmail.com>
2017-08-01 21:36:22 +0200

    proper management of jd2 exit

--- dist/jdownloader.sh
+++ dist/jdownloader.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+function stopJDownloader {
+  PID=$(cat $JDOWNLOADER_HOME/JDownloader.pid)
+  kill $PID
+  wait $PID
+  exit
+}
+
+trap stopJDownloader EXIT
+
 java -Djava.awt.headless=true -jar $JDOWNLOADER_HOME/JDownloader.jar &
 
 while true; do
